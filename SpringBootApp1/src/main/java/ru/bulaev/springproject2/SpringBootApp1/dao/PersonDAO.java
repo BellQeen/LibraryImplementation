bb2324a2dbfb98c24f51e8729f1290d1261c1e6c
Maxim Bulaev <99556851+mabulaev@users.noreply.github.com>
2024-02-08 12:20:48 +0300

    Delete SpringBootApp1 directory

--- SpringBootApp1/src/main/java/ru/bulaev/springproject2/SpringBootApp1/dao/PersonDAO.java
+++
@@ -1,9 +0,0 @@
-package ru.bulaev.springproject2.SpringBootApp1.dao;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class PersonDAO {
-    // Здесь будут лежать специфические сложные запросы к БД (с помощью SQL)
-}
-
